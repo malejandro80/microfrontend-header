2c7ab7ba61d581542f7365b6043bc7ba90fc5723
Miguel Bastidas <mbastidas@abatechnology.com>
2023-06-23 22:59:03 -0500

    change

--- src/example/Example.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import stl from './styles/index.module.scss';
-
-export const Example = () => {
-  console.log(stl, 'estilos');
-  return <h1 className={`${stl['color-blue']} ${stl.small_text}`}>Hello World!</h1>;
-};
